6f06ccef31dfa4f4dd4b35cde7d71b1bd5ec6e25
Erjok17 <mrerjok@gmail.com>
2024-12-09 13:27:58 +0300

    learning 06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning06 World!");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 5, 6),
+            new Circle("Green", 3)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
     }
 }
